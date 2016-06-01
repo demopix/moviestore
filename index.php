a208d7fd62e161007a029dedaec6ba7446a6220f
demopix <vicentedemetrio@gmail.com>
2016-06-01 16:06:20 +0200

    inc/connexion.php

--- index.php
+++ index.php
@@ -5,9 +5,31 @@
 	<title>Movie Store</title>
 </head>
 <body>
+<nav>
+	<ul><li><a href="acc">accueil</a></li><li><a href="acc">Accueil</a></li><a href="add">Catégories</a></li></ul>
+</nav>
+
+<?php if(isset($_GET['add']))
+{
+	include_once'insert_movie.php';
+	} 
+	if(isset($_GET['acc']))
+{
+	include_once'accueil.php';
+	} 
 
 
 
+
+	?>
+
+<footer>
+	<div style="    bottom: -2%;
+    position: absolute;
+}">
+		<p>developres <strong>Gabriela<a href=""></a></strong> <strong>Florian<a href=""></a></strong> <strong>Anne-Marie<a href=""></a></strong> <strong>Demétrio<a href=""></a></strong></p>
+	</div>
+</footer>
 
 </body>
 </html>
