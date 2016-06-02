1d5dae82c66d2d86cea1aef9bda2fc5137808063
itsfgabi <itsfgabi@gmail.com>
2016-06-02 14:23:42 +0200

    dsfsdf<
    dsqwQSDFLKQJHSFIOMSIOFM

--- inc/db.php
+++ inc/db.php
@@ -1,12 +1,13 @@
 <?php
 
-//je me conect à la base de données
+//je me conect a la base de donnes
 $dsn = 'mysql:host=192.168.210.84;dbname=afdg_sql1;charset=utf8';
 
+//on teste si la connexion funcione avec un try
 try{
-	$pdo = new PDO($dsn, 'afdg', 'webforce3');
+	$pdo = new PDO($dsn, 'afdg', 'webforce3'); //on appelle le dsn, et indique le user et la password correspondent
 }
-catch(Exception $error){
+catch(Exception $error){ //si la conexion marche pas on auras un erreur
 	echo 'error';
 	exit;
 }
