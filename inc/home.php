920c2dd9cd0059621eb7d81e84d8354eaca07e07
annemarieyim <annemarie_yim@yahoo.fr>
2016-06-06 12:15:58 +0200

    nouveau fichier home
    le code doit être identique pour home  AnneMarie et catalogue Demetrio

--- inc/home.php
+++ inc/home.php
@@ -5,128 +5,195 @@
 	<title>Home</title>
 	<style type="text/css">
 	HTML, BODY{
-
-	font-family: Verdana, Tahoma, sans-serif;
-
-	}
-
-	.formulaire{
-		width=100%;
+		font-family: Verdana, Tahoma, sans-serif;
+		background-color: grey;
+		color:white;
+		margin: 0px auto;
 	}
 
 	ul li{
-	display: inline-block;
+		display: inline-block;
+		text-decoration: none;
 	}
-	
+
+	input{
+		height: 90px;
+	}
+
+	#bouton{
+		height: 90px;
+		width: 90px;
+	}
 
 	.affichage1, .affichage2,.affichage3,.affichage4 {
 		width:200px;
 		height:200px;
 		border:1px solid red;
 		display: inline-block;
-		margin-left:40px;
-		margin-right:10px;
+		margin-left:35px;
+		margin-right:30px;
 
 	}
+	.alignement{
+		display: inline-block;
+	}
+	a{
+		text-decoration: none;
+	}
+	p {
+		text-align: justify;
+		margin-left:10px 10px;
+	}
+
+
 
 	</style>
 </head>
 	
-</html><body>
 
-	<!--
-
-
-	Accueil La page d’accueil présente brièvement le concept et affiche en premier lieu un grand et gros champ de recherche parmi les films. Une liste non exhaustive de catégories est affichée. Le nombre de films de chaque catégorie (affichée sur l’accueil) est mis entre parenthèses à droite du nom de la catégorie. Les derniers films ajoutés seront aussi affichés avec leur affiche et leur nom.  
-	En option Les 5 ou 6 dernières recherches sont affichées sous la barre de recherche en petits caractères. Un clic sur ces anciennes recherches remplit alors la barre de recherche avec ses termes.  
-	Recherche La recherche simple s’effectue sur les informations suivantes : ● Titre ● Catégorie ● Synopsis ● Nom du fichier  
-	En option La recherche s’effectuera aussi sur : ● Description longue ● Acteur(s)/Actrice(s)  
-	Recherche avancée (en option) Dans la recherche avancée, on peut filtrer/rechercher les résultats par : ● titre ● catégorie (liste) ● acteur/actrice ● support ● date d’ajout à la DB  
-	Résultats de recherche Les résultats de recherche sont affichés comme une page classique du catalogue. La mention “Résultat(s) pour : le texte de la recherche soumise“ est ajoutée en haut, après le header. 
-
-
-
-	-->
+<body>
+	
 	<center>
 
 	<!--Présentation brève du concept-->
-	<h1>Concept</h1>
+	<h1>BIENVENUE SUR MOVIESTORE!</h1>
+	<br/>
 
-	<p>
+	<p >
 
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	Bienvenue sur MovieStore! Cette base de données privée permet d'effectuer une recherche rapide de films par titre, catégorie, synopsis, ou bien encore nom de fichier.<br/>
+	Une recherche rapide optionnelle peut être effectuée selon la description longue ou les acteurs/actrices.
+	Une recherche avancée peut être également effectuée selon le titre, la catégorie, l'acteur/actrice, le support de stockage ou encore la date d'ajout à la base de donnée.<br/>
+	A tout moment, vous pouvez cliquer sur l'onglet <strong>Catégories</strong>, pour effectuer une recherche de films par catégorie, ou bien encore cliquer sur l'onglet <strong>Ajouter un film</strong>, pour rajouter un film à votre base de données.
 
 	</p>
 
 
 	<!--Formulaire de recherche du film-->
+	<br/>
 	<h1>  Rechercher un film</h1>
+	<br/>
 
 	<div class="formulaire">
-		<form action="" method="post">
-		<input type="text" name="rechercheFilm" value="" placeholder="Recherche">
-		<input type="submit" value="OK"><br />
+		<form action="catalogue.php" method="get">
+		<input type="text" name="q" value="" placeholder="Recherche"   style="font-size:40px; color:black" size="38">
+		<input type="submit" value="OK" id="bouton"><br />
 		</form>
 	</div>
 
-
-		<select name="mov_title">
-				<option value="0">choisissez :</option>
-				<?php foreach ($countriesList as $key=>$value) :?>
-				<option value="<?= $key ?>"><?= $value ?></option>
-				<?php endforeach; ?>
-			</select><br />
-
-
-
-
-
 	<!--Liste des films-->
-	<h1>Liste non exhaustive des films par catégorie</h1>
+	<br/>
+	<h1>Liste des films par catégorie</h1>
 
-	<ul>
-		<li><a href="">Science-Fiction</a></li>
-		<li><a href="">Comédie</a></li>
-		<li><a href="">Aventure</a></li>
-		<li><a href="">Thriller</a></li>
-	</ul>
+<!--CODE RECHERCHE FILM PAR TITRE, NOM, SYNOPSIS et FICHIER-VOIR DEMETRIO ET CATALOGUE.PHP
+<?php
+/*
+require 'db.php';
+$resultsSearch=array();//variable à remplir
+//Mettre ici le code pour la recherche
+print_r($_GET);
+if (!empty($_GET['search'])){
+	$searchID=$_GET['search'];
+	$searchAll='%'.$searchID.'%';
+	//echo $searchID;
 
-	<div class="affichage1">
-	<p>Affiche film</p>
+$sqlSearch='
+
+SELECT mov_id, mov_title, cat_name, mov_synopsis, mov_path
+FROM movie
+INNER JOIN category ON category.cat_id = movie.cat_id 
+WHERE mov_title LIKE :searchKeyWord
+OR cat_name LIKE :searchKeyWord 
+OR mov_synopsis LIKE :searchKeyWord
+OR mov_path LIKE :searchKeyWord 
+
+';
+
+$pdoStatement = $pdo->prepare($sqlSearch);
+	$pdoStatement->bindValue(':searchKeyWord', $searchAll, PDO::PARAM_STR);
+
+	// Si erreur
+if ($pdoStatement ->execute()===false) {
+		print_r($pdo->errorInfo());
+	}
+	else if ($pdoStatement->rowCount() > 0) {
+		$resultsSearch = $pdoStatement->fetchAll();
+		print_r($resultsSearch);
+	}
+}
+
+*/
+?>
+FIN COMENTAIRE-->
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require 'db.php';
+$nbMovieCat=array();
+$sql1='
+		SELECT COUNT(*) AS total, cat_name
+		FROM movie
+		LEFT OUTER JOIN category ON category.cat_id = movie.cat_id 
+		GROUP BY cat_name
+	';
+
+$pdoStatement = $pdo->prepare($sql1);
+	
+	// Si erreur
+if ($pdoStatement ->execute()===false) {
+	print_r($pdo->errorInfo());
+}
+else if ($pdoStatement->rowCount() > 0) {
+	$nbMovieCat = $pdoStatement->fetchAll();
+		//print_r($nbMovieCat);
+}
+?>
+
+<ul>
+	<?php foreach ($nbMovieCat as $nbmovie): ?>
+		<li><button><?=$nbmovie['cat_name'].'('.$nbmovie['total'].')'?></button></li>
+	<?php endforeach ?>			
+</ul>
+
+
+<?php
+require 'db.php';
+$movieTitle=array();
+//echo $searchID;
+$sql2='
+	SELECT * 
+	FROM movie
+	LIMIT 4
+';
+
+$pdoStatement = $pdo->prepare($sql2);
+// Si erreur
+if ($pdoStatement ->execute()===false) {
+	print_r($pdo->errorInfo());
+}
+else if ($pdoStatement->rowCount() > 0) {
+	//print_r($movieTitle);
+}
+	$movieTitle = $pdoStatement->fetchAll();
+?>
+
+<?php foreach ($movieTitle as $titleListe): ?>
+	<div class="alignement">
+		<img src="<?=$titleListe['mov_image']?>" width="200px" height="300px"><br/><br/>
+		<button ><?=$titleListe['mov_original_title']?></button>
 	</div>
-
-	<div class="affichage2">
-		<p>Affiche film</p>
-	</div>
-
-	<div class="affichage3">
-		<p>Affiche film</p>
-	</div>
-
-	<div class="affichage4">
-		<p>Affiche film</p>
-	</div>
-
-
-	<center>
-	<table style="width:70%">
-	<center>
-		<tr>
-			<td><a href="">Titre1</a></td>
-			<td><a href="">Titre2</a></td>
-			<td><a href="">Titre3</a></td>
-			<td>T<a href="">itre4</a></td>
-		</tr>
-	</center>
-	</table>
-
-	<h1>Résultats de la Recherche soumise</h1>
-
+<?php endforeach ?>
 	</center>
 	</body>
+
+</html>
 
